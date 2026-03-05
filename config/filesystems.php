a8b5f50aae9c164384f5359d64657a162c8902bd
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2026-03-05 14:58:58 -0300

    Base Domain

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,10 +56,10 @@ return [
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'root' => env('AWS_PREFIX', ''),
             'throw' => false,
             'report' => false,
         ],
-
     ],
 
     /*
