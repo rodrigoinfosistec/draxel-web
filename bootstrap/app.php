e8183fcfd50aa928411b7e1bca14a54750a23420
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2026-03-05 13:24:42 -0300

    Base Project

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
 use App\Http\Middleware\IdentifyTenant;
 use App\Http\Middleware\SetCompanyContext;
+use App\Http\Middleware\TrustProxies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -17,6 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
+
+        $middleware->append(TrustProxies::class);
 
         $middleware->alias([
             'requireTenant' => \App\Http\Middleware\RequireTenant::class,
