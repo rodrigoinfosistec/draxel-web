44cb1b22d4be83845e20e8d25333e0792f24e67e
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2026-03-07 13:05:38 -0300

    Base Project

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,10 @@
 
 namespace App\Providers;
 
-use App\Models\Tenant;
-use App\Observers\TenantObserver;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
@@ -29,6 +28,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
 
         // Tenant::observe(TenantObserver::class);
     }
