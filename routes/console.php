0df136e7399d171d1e9c4ae58b3673b7d0fd9dce
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2026-03-14 20:28:09 -0300

    remove filament backup dump routine

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Jobs\RunDatabaseBackup;
-use App\Models\DatabaseBackup;
-use Illuminate\Support\Facades\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -10,13 +7,3 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::call(function () {
-    $backup = DatabaseBackup::query()->create([
-        'type' => 'scheduled',
-        'status' => 'pending',
-        'disk' => 's3',
-        'requested_by' => null,
-    ]);
-
-    RunDatabaseBackup::dispatch($backup->id);
-})->dailyAt('03:00');
